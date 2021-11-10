18cdadadba7bf0a1eae48059d88efe4e2532a149
Sangeetha90 <Sangeetha.Kadasani@ness.com>
2021-11-10 09:54:36 +0530

    some, fine, every task submit changes

--- task_day1.js
+++ task_day1.js
@@ -1,1 +1,27 @@
-var a =1;
+const users = [{name: 'yagnesh',age: 30,gender: 'male'},{name: 'virat',age: 28,gender: 'male'},{name: 'rohit',age: 32,gender: 'male'},{name: 'dipika',age: 34,gender: 'female'},{name: 'alia',age: 19,gender: 'female'},{name: 'rekha',age: 64,gender: 'female'},{name: 'Taimur',age: 8,gender: 'male'}]
+
+const someData =users.reduce((p,c)=>{
+    if (c.name === 'rohit'){
+        return true;
+    }
+    return p;
+}, false);
+console.log(someData);
+const findData =users.reduce((p,c)=>{
+    if (c.name === 'rohit'){
+        return c;
+    }
+    return p;
+}, {});
+console.log(findData);
+const everyData =users.reduce((p,c)=>{
+    console.log(c);
+    if (c.age > 18 && p) {
+        return true;
+    } else {
+        return false;
+    }    
+    return p;
+}, true);
+console.log(everyData);
+
